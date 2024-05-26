e7a4f300d27c9bf25fd3be3fe6f1468f456ee363
Berlandro <bryangodlive0@gmail.com>
2024-05-26 21:11:36 +0700

    Routes: add testimonial router

---
+++ routes/testimonialRoutes.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import Testimonial from '../models/testimonialModel.js';
+import Construction from '../models/constructionModel.js';
+const testiRouter = express.Router();
+
+testiRouter.get('/testimonials', async(req, res) => {
+    try {
+        const allTestimonials = await Testimonial.find({});
+
+        res.status(200).json(allTestimonials)
+    } catch(err) {
+        res.status(500).json({message: err.message});
+    }
+})
+
+testiRouter.post('/constructions/:constructionId/testimonials', async(req, res) => {
+    const params = req.params.constructionId;
+
+    try{
+        const construct = await Construction.findById(params);
+        if (!construct) {
+            return res.status(404).json({ message: "Produk tidak ditemukan" });
+        }
+
+        const newTestimonials = new Testimonial({construction: params, ...req.body});
+        const sendTestimonials = await newTestimonials.save()
+        construct.reviews.push(sendTestimonials._id);
+
+        await construct.save()
+        res.status(200).json(newTestimonials);
+    } catch(err) {
+        res.status(500).json({message: err.message});
+    }
+})
+
+export default testiRouter;
