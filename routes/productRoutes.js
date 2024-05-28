b46f523df1f31bb65c3dda7d3611e98e30cfc651
Berlandro <bryangodlive0@gmail.com>
2024-05-28 14:16:58 +0700

    routes: add /products/upload path for upload data and image

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import Product from '../models/productModel.js';
 import { addProduct, deleteProduct, getAllProducts, getProduct, updateProduct } from '../controllers/products.js';
+import { storage } from '../config/cloudinaryConfig.js';
+import multer from 'multer';
 const prodRouter = express.Router();
+const upload = multer({storage: storage})
 
 prodRouter.get('/products', getAllProducts)
-prodRouter.post('/products', addProduct)
+prodRouter.post('/products/upload', upload.single('photo'), addProduct)
 prodRouter.get('/products/:prodId', getProduct)
 prodRouter.patch('/products/:prodId', updateProduct)
 prodRouter.delete('/products/:prodId', deleteProduct)
