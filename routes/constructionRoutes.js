3868cb71ebfecea08c9058a016f0a649ca10f28b
Berlandro <bryangodlive0@gmail.com>
2024-05-28 14:16:20 +0700

    routes: add /constructions/upload path for upload data and img

--- routes/constructionRoutes.js
+++ routes/constructionRoutes.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import Construction from '../models/constructionModel.js';
 import getDuration from '../utils/getDuration.js';
+import { storage } from '../config/cloudinaryConfig.js';
+import multer from 'multer';
 const constructRouter = express.Router();
+const upload = multer({storage: storage})
 
 constructRouter.get('/constructions', async(req, res) => {
     try {
@@ -12,17 +15,19 @@ constructRouter.get('/constructions', async(req, res) => {
     }
 })
 
-constructRouter.post('/constructions', async(req, res) => {
+constructRouter.post('/constructions/upload', upload.single('photo'), async(req, res) => {
     const {start, finish, ...rest} = req.body
     const sumDuration = await getDuration(start, finish)
+    const imgUrl = req.file.path
 
     try{
         const newConstruction = new Construction({
-            project_timeline: {start, finish, duration: sumDuration}, 
+            project_timeline: {start, finish, duration: sumDuration},
+            image: imgUrl,
             ...rest
         })
         await newConstruction.save();
-        res.status(200).json(newConstruction)
+        res.status(200).json({message: 'Success adding data', status: 200, data: newConstruction})
     }catch(err){
         res.status(500).json({message: err.message});
     }
