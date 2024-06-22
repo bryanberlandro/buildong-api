52525873801abeabbfe8bc89f14b44cf6f43d45d
Berlandro <bryangodlive0@gmail.com>
2024-06-22 12:14:43 +0700

    CORS : fixing cors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors(
   {
-      origin: ['http://localhost:5173', 'https://buildong-api.vercel.app/', 'https://buildong-pi.vercel.app'],
+      origin: ['http://localhost:5173/', 'https://buildong-api.vercel.app/', 'https://buildong-pi.vercel.app'],
       methods: ["POST", "GET", "PATCH", "DELETE"],
       credentials: true
   }
