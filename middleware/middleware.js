60ed752bdbab0cc77929cef3e42cb2de42db1cd0
Berlandro <bryangodlive0@gmail.com>
2024-06-22 16:52:40 +0700

    MIDDLEWARE : change secret key

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken';
+const SECRET_KEY = process.env.SECRET_KEY
 
 export const verifyToken = (req, res, next) => {
     const token = req.headers['authorization']?.split(' ')[1];
@@ -8,11 +9,10 @@ export const verifyToken = (req, res, next) => {
     }
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.userId = decoded.id;
-        req.userType = decoded.role;
-        console.log(decoded.id)
-        console.log(decoded.role)
+        const decoded = jwt.verify(token, process.env.SECRET_KEY);
+        req.userId = decoded.user.id;
+        req.userType = decoded.user.role;
+        console.log("decoded: " + decoded)
         next();
     } catch (error) {
         return res.status(401).json({ message: 'Token tidak valid' });
