72e6c3617a8952808d78b4c80a65de4404c6169f
Berlandro <bryangodlive0@gmail.com>
2024-06-22 16:11:54 +0700

    ROUTES : fix some routes at login

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -76,66 +76,45 @@ authRouter.post('/login', async (req, res) => {
     const { email, password } = req.body;
     try {
         const validation = await loginValid(email, password);
-        if(validation.msg.length > 0){
-            return res.status(400).json({message: validation.msg})
+        if (validation.msg.length > 0) {
+            return res.status(400).json({ message: validation.msg });
         }
-        
-        let admin = await Admin.findOne({ email });
-        if(admin){
-            const isMatch = await bcrypt.compare(password, admin.password);
-            if(!isMatch){
-                return res.status(400).json({message: 'Invalid Password'}) 
-            }
-            const payload = {
-                user: {
-                    id: admin._id,
-                    email: admin.email,
-                    role: "admin"
-                }
-            }
 
-            jwt.sign(payload, SECRET_KEY, { expiresIn: '3d' }, (err, token) => {
-                if (err) throw err;
-                res.json({ 
-                    status: "200",
-                    message: 'Login successful! Welcome back '+user.email,
-                    token: token,
-                    role: "admin"
-                });
+        let user = await Admin.findOne({ email });
+        if (!user) {
+            user = await User.findOne({ email });
+            if (!user) {
+                return res.status(400).json({ message: 'Email not registered. Please sign up to create an account' });
+            }
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Invalid Password. Please check your password and try again' });
+        }
+
+        const role = user instanceof Admin ? 'admin' : 'user';
+        const payload = {
+            user: {
+                id: user._id,
+                email: user.email,
+                role: role
+            }
+        };
+
+        jwt.sign(payload, SECRET_KEY, { expiresIn: '3d' }, (err, token) => {
+            if (err) throw err;
+            res.json({
+                status: "200",
+                message: `Login successful! Welcome back ${user.email}`,
+                token: token,
+                role: role
             });
-            return;
-        }
-
-        let user = await User.findOne({ email });
-        if(user){
-            const isMatch = await bcrypt.compare(password, user.password);
-            if(!isMatch){
-                return res.status(400).json({message: 'Invalid Password. Please check your password and try again'})
-            }
-            const payload = {
-                user: {
-                    id: user._id,
-                    email: user.email,
-                    role: "user"
-                }
-            }
-
-            jwt.sign(payload, SECRET_KEY, { expiresIn: '3d' }, (err, token) => {
-                if (err) throw err;
-                res.json({ 
-                    status: "200",
-                    message: 'Login successful! Welcome back '+user.email,
-                    token: token,
-                    role: "user"
-                });
-            });
-            return;
-        }
-
-        return res.status(400).json({ message: 'Email not registered. Please sign up to create an account' });
-    } catch(err){
-        console.log(err)
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server Error' });
     }
-})
+});
 
 export default authRouter
