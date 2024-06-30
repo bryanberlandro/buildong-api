157f9edbd023309f9505ea95042c08be0ae61a9f
Berlandro <bryangodlive0@gmail.com>
2024-06-30 14:49:31 +0700

    FEAT : Admin

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { verifyToken } from '../middleware/middleware.js';
 import Admin from '../models/adminModel.js';
+import ConstructionOrder from '../models/constructOrderModel.js';
+import ProductOrder from '../models/productOrderModel.js';
 const adminRouter = express.Router();
 
 adminRouter.get('/admin', verifyToken, async(req, res) => {
@@ -20,5 +22,57 @@ adminRouter.get('/admin', verifyToken, async(req, res) => {
         res.status(500).json({error: error.message})
     }
 })
+
+adminRouter.get('/admin/construction-orders/completed', verifyToken, async (req, res) => {
+    try {
+        const isAdminExist = await Admin.findById(req.userId, { password: 0 });
+        if (!isAdminExist) {
+            return res.status(404).json({ msg: "User not found" });
+        }
+
+        const completedOrders = await ConstructionOrder.find({ status: 'Completed' });
+        if(completedOrders.length > 0){
+            res.status(200).json({
+                msg: "Successfully get completed construction orders",
+                status: 200,
+                data: completedOrders,
+                method: req.method
+            });
+        } else {
+            res.status(404).json({
+                msg: "There is no completed construction orders",
+                status: 404,
+            });
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+adminRouter.get('/admin/product-orders/completed', verifyToken, async (req, res) => {
+    try {
+        const isAdminExist = await Admin.findById(req.userId, { password: 0 });
+        if (!isAdminExist) {
+            return res.status(404).json({ msg: "User not found" });
+        }
+
+        const completedOrders = await ProductOrder.find({ status: 'Completed' });
+        if(completedOrders.length > 0){
+            res.status(200).json({
+                msg: "Successfully get completed product orders",
+                status: 200,
+                data: completedOrders,
+                method: req.method
+            });
+        } else {
+            res.status(404).json({
+                msg: "There is no completed product orders",
+                status: 404,
+            });
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
 
 export default adminRouter;
