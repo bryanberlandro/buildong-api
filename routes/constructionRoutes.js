26476e58e49ccb26f2a94f2dd317730fd4b49de8
Berlandro <bryangodlive0@gmail.com>
2024-07-03 11:19:41 +0700

    GET: Update construction and product to get reviews

--- routes/constructionRoutes.js
+++ routes/constructionRoutes.js
@@ -8,6 +8,22 @@ const upload = multer({storage: storage})
 
 constructRouter.get('/constructions', getAllConstructions)
 constructRouter.post('/constructions/upload', upload.array('photo', 3), postConstruction)
+constructRouter.get('/:constructionId/reviews', async (req, res) => {
+    try {
+        const construction = await Construction.findById(req.params.constructionId).populate('reviews');
+        if (!construction) {
+            return res.status(404).json({ message: 'Construction not found' });
+        }
+        res.status(200).json({
+            status: 200,
+            message: "Successfully get "+construction.reviews.length+ " reviews",
+            data: construction.reviews,
+            method: req.params.method
+        });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
 constructRouter.get('/constructions/category/:category', async (req, res) => {
     try {
         const { category } = req.params;
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -9,6 +9,23 @@ const upload = multer({storage: storage})
 prodRouter.get('/products', getAllProducts)
 prodRouter.post('/products/upload', upload.array('photo', 3), addProduct)
 prodRouter.get('/products/:prodId', getProduct)
+prodRouter.get('/:productId/reviews', async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.productId).populate('reviews');
+        if (!product) {
+            return res.status(404).json({ message: 'Product not found' });
+        }
+
+        res.status(200).json({
+            status: 200,
+            message: "Successfully get "+product.reviews.length+" reviews",
+            data: product.reviews,
+            method: req.params.method
+        });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
 prodRouter.patch('/products/:prodId', updateProduct)
 prodRouter.delete('/products/:prodId', deleteProduct)
 
